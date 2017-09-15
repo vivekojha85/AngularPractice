2b2508c7c28e21df8174eddb2c23d2bfe9154c2b
vivekojha85 <vivekojha85@gmail.com>
2017-09-14 21:53:24 -0400

    Added Radio button

--- AngularProjects/forms-td-start/src/app/app.component.ts
+++ AngularProjects/forms-td-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,20 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   defaultUserName = 'Vivekojha85';
   answer='';
+  genders = ['male','female'];
 
-  onSubmit(form: NgForm){
-    console.log(form);
+  //onSubmit(form: NgForm){
+    //console.log(form);
+  //}
+
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
