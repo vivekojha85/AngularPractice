4d8e9ea6550f4f08f80bcb3f964ed323d0580c2a
vivekojha85 <vivekojha85@gmail.com>
2017-09-17 08:59:23 -0400

    Added Http Post method

---
+++ AngularProjects/http-start/src/app/server.service.s.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by Vivek Ojha on 9/17/17.
+ */
+
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+
+@Injectable()
+export class ServerServiceS{
+
+  constructor(private http: Http){}
+
+  storeServers(server: any[]){
+    const headers = new Headers({'Content-Type': 'application/json','entityName':'abc'});
+  return  this.http.post('https://myfirstproject-690bd.firebaseio.com/data.json',
+    server,
+    {headers: headers});
+  }
+}
